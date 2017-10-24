002c93882b9f8be57d647a0d1d0544aca529e19c
Tarek Samaali <tarek.samaali.92@gmail.com>
2017-10-24 20:45:52 +0100

    Update Optimizers.py

--- src/Optimizers.py
+++ src/Optimizers.py
@@ -6,12 +6,9 @@ class SGD:
         self.momentum = .01
         self.learningrate=.01
         self.w_update = None
-        # If not initialized
         if not self.w_update :
             self.w_update = np.zeros_like(w)
-        # Use momentum if set
         self.w_update = self.momentum * self.w_update + (1 - self.momentum) * loss_deriv
-        # Move against the gradient to minimize loss
         return w - self.learningrate * self.w_update
 
 
